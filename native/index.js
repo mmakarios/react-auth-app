cd299bd6ac7c630312071788d2bcd7f75cd8bade
Michel Makarios <michel.makarios@gmail.com>
2019-12-18 19:58:44 -0300

    Fix react-navigation installation

--- native/index.js
+++ native/index.js
@@ -7,5 +7,6 @@ import { AppRegistry } from 'react-native';
 // https://facebook.github.io/react-native/docs/platform-specific-code#native-specific-extensions-ie-sharing-code-with-nodejs-and-web
 import Root from '../src/components/Root';
 import { name as appName } from './app.json';
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => Root);
