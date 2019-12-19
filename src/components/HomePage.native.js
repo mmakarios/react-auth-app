7b42eda5c244893fe7ad5efa95ea3ca79898e8c3
Michel Makarios <michel.makarios@gmail.com>
2019-12-19 14:40:28 -0300

    Add dishes reducer to mobile

--- src/components/HomePage.native.js
+++ src/components/HomePage.native.js
@@ -4,16 +4,27 @@ import { View, Text, Button } from 'react-native';
 
 import styles from '../styles/homePage.js';
 import * as screens from '../constants/screens';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getDishes } from '../reducers/dishesReducer.js';
 
 // import { Container, Content, Button } from 'native-base';
 
 class HomePage extends React.Component {
+  componentDidMount() {
+    const { data, getDishes } = this.props;
+
+    if (!data) {
+      getDishes();
+    }
+  }
+
   render() {
-    const { navigation } = this.props;
+    const { navigation, data } = this.props;
     return (
       <View>
         <Text style={styles.sectionDescription}>
-          DISHES LIST WHEN LOGGED IN
+          {data && data.dishes[0].description}
         </Text>
         <Button
           title="Go to login page"
@@ -34,6 +45,24 @@ class HomePage extends React.Component {
 
 HomePage.propTypes = {
   navigation: PropTypes.object.isRequired,
+  getDishes: PropTypes.func,
+  isFetching: PropTypes.bool,
+  error: PropTypes.bool.isRequired,
+  data: PropTypes.object,
 };
 
-export default HomePage;
+const mapStateToProps = state => {
+  return {
+    isFetching: state.dishes.isFetching,
+    error: state.dishes.error,
+    data: state.dishes.data,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getDishes: bindActionCreators(getDishes, dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- src/reducers/index.native.js
+++ src/reducers/index.native.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import loginReducer from './loginReducer';
 import signUpReducer from './signUpReducer';
+import dishesReducer from './dishesReducer';
 
 const rootReducer = () =>
   combineReducers({
     login: loginReducer,
     signUp: signUpReducer,
+    dishes: dishesReducer,
   });
 
 export default rootReducer;
