7f58f9398142385089c7d208798b1b2283d52b60
Michel Makarios <michel.makarios@gmail.com>
2019-12-19 01:21:05 -0300

    Fix dishes list endpoint

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,3 +1,3 @@
 export const LOGIN = 'http://www.mocky.io/v2/5defab092f0000e7848e0c9e';
 export const SIGN_UP = 'http://www.mocky.io/v2/5defab092f0000e7848e0c9e';
-export const USER_DISHES = ' http://www.mocky.io/v2/5df018d52f0000abc18e0f22';
+export const USER_DISHES = 'http://www.mocky.io/v2/5dfaf6792f00007900ff9b41';
