28663302f96cc0e40c0f991e9e5da768f54197e0
Michel Makarios <michel.makarios@gmail.com>
2019-12-19 22:19:39 -0300

    Fix iOS rendering

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import { AppRegistry } from 'react-native';
 // Automatically fetches Root.native.js by detecting build platform
 // https://facebook.github.io/react-native/docs/platform-specific-code#native-specific-extensions-ie-sharing-code-with-nodejs-and-web
-import Root from '../src/components/Root';
+import Root from './src/components/Root';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
 
--- ios/build/reactnativetest/Build/Intermediates.noindex/reactnativetest.build/Debug-iphonesimulator/reactnativetest.build/Objects-normal/x86_64/reactnativetest_vers.d
+++ ios/build/reactnativetest/Build/Intermediates.noindex/reactnativetest.build/Debug-iphonesimulator/reactnativetest.build/Objects-normal/x86_64/reactnativetest_vers.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Users/guiconti/github/reactnativetest/ios/build/reactnativetest/Build/Intermediates.noindex/reactnativetest.build/Debug-iphonesimulator/reactnativetest.build/DerivedSources/reactnativetest_vers.c
+  /Users/Michel/Documents/PROJETOS/react-auth-app/ios/build/reactnativetest/Build/Intermediates.noindex/reactnativetest.build/Debug-iphonesimulator/reactnativetest.build/DerivedSources/reactnativetest_vers.c
